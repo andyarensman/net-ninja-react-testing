2e44ee302597c6e286d1c1ae79ace590ffd345db
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-06-03 15:45:10 -0700

    lesson 8 - assertions

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -13,32 +13,32 @@ it('should render same text passed into title prop', () => {
 //   expect(headingElement).toBeInTheDocument();
 // });
 
-it('header get by role with speicifc title', () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.getByRole("heading", { name: "My Header"});
-  expect(headingElement).toBeInTheDocument();
-});
-
-//Find by
-
-it('header findybytext with async await', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = await screen.findByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
-});
-
-//Query by
-
-it('header is not dogs', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.queryByText(/dogs/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
-
-//get all by
-
-it('header amount is equal to 1', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.getAllByRole("heading");
-  expect(headingElement.length).toBe(1);
-});
+// it('header get by role with speicifc title', () => {
+//   render(<Header title="My Header"/>);
+//   const headingElement = screen.getByRole("heading", { name: "My Header"});
+//   expect(headingElement).toBeInTheDocument();
+// });
+//
+// //Find by
+//
+// it('header findybytext with async await', async () => {
+//   render(<Header title="My Header"/>);
+//   const headingElement = await screen.findByText(/my header/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
+//
+// //Query by
+//
+// it('header is not dogs', async () => {
+//   render(<Header title="My Header"/>);
+//   const headingElement = screen.queryByText(/dogs/i);
+//   expect(headingElement).not.toBeInTheDocument();
+// });
+// 
+// //get all by
+//
+// it('header amount is equal to 1', async () => {
+//   render(<Header title="My Header"/>);
+//   const headingElement = screen.getAllByRole("heading");
+//   expect(headingElement.length).toBe(1);
+// });
