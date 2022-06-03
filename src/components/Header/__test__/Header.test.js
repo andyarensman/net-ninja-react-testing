814061ea19538836224d4ba6c9c890f2bb238e42
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-06-03 15:10:28 -0700

    lesson 7 - using query methods (first commit since setup)

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+it('should render same text passed into title prop', () => {
+  render(<Header title="My Header"/>);
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// it('should render same text passed into title prop', () => {
+//   render(<Header title="My Header"/>);
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+it('header get by role with speicifc title', () => {
+  render(<Header title="My Header"/>);
+  const headingElement = screen.getByRole("heading", { name: "My Header"});
+  expect(headingElement).toBeInTheDocument();
+});
+
+//Find by
+
+it('header findybytext with async await', async () => {
+  render(<Header title="My Header"/>);
+  const headingElement = await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+//Query by
+
+it('header is not dogs', async () => {
+  render(<Header title="My Header"/>);
+  const headingElement = screen.queryByText(/dogs/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+//get all by
+
+it('header amount is equal to 1', async () => {
+  render(<Header title="My Header"/>);
+  const headingElement = screen.getAllByRole("heading");
+  expect(headingElement.length).toBe(1);
+});
